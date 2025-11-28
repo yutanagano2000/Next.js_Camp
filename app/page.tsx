f05710d29443eed0cf0e04669078c9312efc4a49
yuta.nagano <yuta.nagano2000@gmail.com>
2025-11-28 13:08:00 +0900

    Sign up画面 | ダークモード対応とレイアウト調整済み

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,52 @@
 "use client"
-import { CssVarsProvider, FormControl, Typography, FormLabel, Sheet, Input, Button, Link} from "@mui/joy"
+
+import { FormControl, FormLabel, Input, Sheet, Typography, Button, Link } from "@mui/joy"
 
 export default function App() {
-  return (
-    <CssVarsProvider>
-      <Sheet
-      sx={{
-        width:300,
-        mx: 'auto',
-        my: 5,
-        px: 3,
-        py: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        borderRadius: 'md',
-        boxShadow: 'md'
-      }}
-      variant='outlined'>
-        <div>
-          <Typography level="h3" component="h1">Welcome!</Typography>
-          <Typography level="body-sm">Sing in to continue.</Typography>
-        </div>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input
-          name='email'
-          type='email'
-          placeholder="Enter your email."
-          ></Input>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Password</FormLabel>
-          <Input
-          name='password'
-          type='password'
-          placeholder="Enter your password."
-          ></Input>
-        </FormControl>
-        <Button sx={{mt:1}}>
-          Log in
-        </Button>
-        <Typography
-        endDecorator={<Link href="/sign-up">Sign up</Link>}
-        fontSize="sm"
-        sx={{alignSelf: 'center'}}>
-          Don&apos;t have an account?
-        </Typography>
-      </Sheet>
+  return(
+    <Sheet
+    sx={{
+      width: 300,
+      mx: 'auto',
+      my: 3,
+      px: 2,
+      py: 4,
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+      borderRadius: 'md',
+      boxShadow: 'md'
+    }}
+    variant="outlined">
+      <div>
+        <Typography level="h3">Welcome!</Typography>
+        <Typography level="body-sm">Sign in to continue.</Typography>
+      </div>
+      <FormControl sx={{mt:1}}>
+        <FormLabel>Email</FormLabel>
+        <Input
+        name="email"
+        type="email"
+        placeholder="enter your email"
+        ></Input>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <Input
+        name="password"
+        type="password"
+        placeholder="enter your password"></Input>
+      </FormControl>
+      <Button sx={{mt:1}}>Login</Button>
+      <Typography
+      endDecorator={<Link href="/sign-up">Sign up</Link>}
+      level="body-sm"
+      sx={{alignSelf:'center'}}>
+        Don&apos;t have an account?
+      </Typography>
+      
 
-    </CssVarsProvider>
+
+    </Sheet>
   )
 }
